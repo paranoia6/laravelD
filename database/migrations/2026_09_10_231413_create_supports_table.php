7b612718cf98f878af2dd8c997da94d9ffe841ad
paranoia6 <ghanbarimreza6@gmail.com>
2026-09-11 02:47:09 +0330

    feat: support migration

---
+++ database/migrations/2026_09_10_231413_create_supports_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('supports', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->json('meta_data')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('supports');
+    }
+};
