e9d91924204d3b7de83f5397f11ffca9c4ac41c8
paranoia6 <ghanbarimreza6@gmail.com>
2026-09-21 04:18:40 +0330

    fix: vite

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/admin.css',
-                'resources/js/admin.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
